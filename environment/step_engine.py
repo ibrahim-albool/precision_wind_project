0405162bb5d32cc67b218ccf3dfd64bd9ab24d6f
Ibrahim Albool <ibrabool@gmail.com>
2025-02-03 13:41:08 -0800

    Adding Linf norm measure and generating some results

--- environment/step_engine.py
+++ environment/step_engine.py
@@ -45,8 +45,11 @@ class ControlEngine:
 
             L2_error_norm = np.array(env.l2_norm_err_list).flatten() * np.sqrt(env.dt) # dt will be squared in the L2 Norm
             L2_error_norm = np.linalg.norm(L2_error_norm)
+            Linf_error_norm = np.max(env.l2_norm_err_list)
             print(f"L2 error norm {L2_error_norm}")
-            plot_geometric_data(env)
+            print(f"Linf error norm {Linf_error_norm}")
+
+            # plot_geometric_data(env)
 
         # print(f"counter = {env.counter}, reward = {reward}, error_norm = {env.error_norm}")
 
